cae5ec7b4f7afdd9bc980a9e2ca51f1bd8dbd6c9
camilarojs <camilarojasm3@gmail.com>
2026-04-22 14:38:29 -0600

    Add comment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This is a super duper cool comment.
+        // another comment
         Console.WriteLine("Hello Sandbox World!");
     }
 }
