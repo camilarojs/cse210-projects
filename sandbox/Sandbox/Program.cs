8c923af4ab95a2abe39be5092dc85c4d119b7d79
camilarojs <camilarojasm3@gmail.com>
2026-04-28 12:08:06 -0600

    C# Prep 1 Activity done

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is a super duper cool comment.
-        // another comment
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello");
+        // int x = 10;
+        // if (x == 11)
+        // {
+        //     Console.WriteLine("Hey bob");
+        //     Console.WriteLine("Hey Betty");
+        // }
+
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
+        // Console.WriteLine(i);
+
+        int age = -1;
+
+        while (age < 0 || age > 125)
+        {
+            Console.WriteLine("Please enter your age: ");
+            age = int. Parse(Console.ReadLine());
+        }
+
+        int age2;
+
+        do
+        {
+            Console.WriteLine("Please enter your age: ");
+            age2 = int. Parse(Console.ReadLine());
+            while (age2 < 0 || age2 > 125);
+        }
     }
 }
+
+//      //This is a super duper cool comment.
+//      //another comment
+//      //Console.WriteLine("Hello Sandbox World!");
